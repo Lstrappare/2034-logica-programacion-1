781008e184c32869d315d123bc01e4122e6f75c4
Lstrappare <j.m.cisval1@gmail.com>
2024-01-24 22:04:09 -0600

    Terminando proyecto

--- app.js
+++ app.js
@@ -1,30 +1,39 @@
-// Establece el numero secreto 
-let numeroSecreto = 6; 
+// Rango del 1 al numero que ponga el usuario.
+let rango = prompt('Indica el rango del 1 al numero que tu quieras: ');
+// Establece el numero secreto.
+let numeroSecreto = Math.floor(Math.random()*rango) + 1; 
 let numeroUsuario = 0;
-// Inicia el número de intentos
+// Inicia el número de intentos.
 let intentos = 1;
-// Palabra vez iniciada en singular
-let palabraVeces = 'vez';
+// Determina el número máximo de intentos.
+let maximosIntentos = 3;
+
 // Va a crear un blucle hasta que el número coincida.
 while (numeroUsuario != numeroSecreto) {
-    numeroUsuario = prompt('Me indicas un número del 1 al 10?');
-    // Muestra el numero del usuario en consola
+    numeroUsuario = prompt(`Me indicas un número del 1 al ${rango}?`);
+    // Muestra el numero del usuario en consola.
     console.log(numeroUsuario);
 
-    // Verifica el numero secreto
+    // Verifica el numero secreto.
     if (numeroUsuario == numeroSecreto) {
-        alert (`Felicidades, acertaste! tu numero era: ${numeroUsuario}, lo hacertaste en ${intentos} ${palabraVeces}`);
+        alert (`Felicidades, acertaste! tu numero era: ${numeroUsuario}, lo hacertaste en ${intentos} ${intentos == 1 ? 'vez' : 'veces'}`);
     } else {
+        // Pistas para el usuario.
         if (numeroUsuario < numeroSecreto) {
             alert('Cerca! el número secreto es mayor que el número secreto que has puesto.');
         } else if (numeroUsuario > numeroSecreto) {
             alert('Cerca! el número secreto es menor al número que has puesto')
         };
-        // Aumenta el número de intentos
-        intentos = intentos + 1;
-        // Cambia la palabra a plural
-        palabraVeces = 'veces';
 
-        //alert (`Lo siento no has acertado, el numero era: ${numeroSecreto}`);
+        // Aumenta el número de intentos.
+        // intentos = intentos + 1;
+        intentos ++;
+
+        // Cantidad máxima de intentos.
+        if (intentos > maximosIntentos) {
+            alert (`Lo siento no has acertado, llegaste a el número máximo de intentos (${maximosIntentos}), el numero era: ${numeroSecreto}`);
+            // Termina el bucle.
+            break;
+        };
     };
 };
